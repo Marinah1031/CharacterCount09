b7f70bcf94cedd148d110699420d95bc8d6d437f
Marinah1031 <marinahtam@gmail.com>
2023-08-24 19:40:02 -0700

    adding a series of questions and messages regarding the Readme created

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name: 'github',
+        message:"What is your GitHub username?",
+    },
+    {
+        type: 'input',
+        name: "email",
+        message: "Enter email address:",
+    },
+    {
+        type:'input',
+        name: 'title',  // title of project
+        message:"What's the title of your project?",
+    },
+    {
+        type: 'input',
+        name: "description",
+        message: `Please provide a description about what you are building.`,
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message:`Select license`,
+        choices:['MIT', 'APACHE 2.0', 'GPL 3.0', 'BDS 3', 'NONE'],
+    },
+    {
+        type:'input',
+        name: 'installation',   //installation instructions
+        message: 'How do I install it?',
+        default: 'npm i'
+    },
+    {
+        type:'input',
+        name: 'test',
+        message: 'How to run tests?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'usage',    // usage information
+        message: 'What does the user need to know about the repository?:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',     /// contribution guidelines
+        message: 'Who contributed to this project:',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
